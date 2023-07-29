dca34c48de40204f2dd3c61102926b1b50dd1a1f
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-29 13:25:14 -0400

    create trivia question button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Display from "./components/Display";
 import ControlledForm from "./components/ControlledForm";
+import {useState} from "react";
 
 function App() {
+
+  const [question, setQuestion] = useState({});
+
   return (
     <div className="App">
       
-      <Display />
+      <Display question={question}/>
       <ControlledForm />
 
     </div>
--- src/components/ControlledForm.js
+++ src/components/ControlledForm.js
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
 function ControlledForm(props){
-    return <h1>ControlledForm</h1>
+    return <div>
+        <h2>Controlled Form</h2>
+        <button>Random Trivia Question</button>
+    </div>
+
+
 }
 
 export default ControlledForm;
