748b2c2cd4d9611b8daf4971908bf888464e8e64
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-29 17:29:35 -0400

    display answer

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,10 +4,15 @@ function Display(props){
 
     if (question[0]) {
         return <div>
-            
-            <h2>Question: {question[0].question}</h2>
-            <h3> Category: {question[0].category.title}</h3>
-            <h3>Points: {question[0].value}</h3>
+            <div>
+                <h2>Question: {question[0].question}</h2>
+                <h3> Category: {question[0].category.title}</h3>
+                <h3>Points: {question[0].value}</h3>
+            </div>
+
+            <div>
+                <h2>Answer: {question[0].answer}</h2>
+            </div>
         </div>
     }else {
         return <h1>Try again</h1>
